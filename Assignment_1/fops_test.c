612c3cd096da39f8fa5ba09db5dc15b96202d94a
MitchellOethChapman <oeth@chapman.edu>
2024-09-22 04:08:13 +0000

    works

--- Assignment_1/fops_test.c
+++ Assignment_1/fops_test.c
@@ -11,19 +11,112 @@ supported operations include:
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <string.h>
+
 #include "fops.h"
 #include "libfops.a"
 
-
 int main (int argc, char *argv[])
 {
-    char input[] = "";
-    while(1)
-    {
+    int action; // stores the action the use would like to take
+    char filename[100]; // stores the command line input
+    int flags;
+    mode_t mode = 0644;
+    int fd;
+    int running = 1;
 
-        scanf("%s", &input);
+    while(running == 1)
+    {
+        printf("What would you like to do?\n");
+        printf("Type the corresponding number to initiate a command:\n");
+        printf("1. Create a new file or open an existing file.\n");
+        printf("2. Write to a file.\n");
+        printf("3. Read a file.\n");
+        printf("4. Close a file.\n");
+        printf("5. Delete a file.\n");
+        printf("6. Quit\n");
+
+        if (scanf("%i", &action) != 1) {
+            printf("You didn't enter an integer.\n");
+            while (getchar() != '\n'); // Clear input buffer
+        }
         
-        break;
+        switch (action) {
+            case 1: // create or open a file
+                printf("What is the name of the file?\n");
+                scanf("%s", filename);
+                flags = O_WRONLY | O_CREAT | O_TRUNC;
+                fd = create_file(filename, flags, mode);
+                if(fd < 0) { // error checking
+                    perror("open failed");
+                    return -1;
+                }
+                break;
+            case 2: // write to a file
+                printf("What is the name of the file?\n");
+                scanf("%s", filename);
+                flags = O_WRONLY;
+                fd = create_file(filename, flags, mode);
+                if(fd < 0) { // error checking
+                    perror("open failed");
+                    return -1;
+                }
+                printf("What would you like to write to the file?\n");
+                char writeData[100];
+                scanf("%s", writeData);
+                ssize_t bytesWritten = write_to_file(fd, writeData, strlen(writeData));
+                if(bytesWritten < 0) { // error checking
+                    perror("write failed");
+                    close_file(fd);
+                    return -1;
+                } else {
+                    printf("Bytes written to %s: %zd\n", filename, bytesWritten);
+                }
+                close_file(fd);
+                break;
+            case 3: // read a file
+                printf("What is the name of the file?\n");
+                scanf("%s", filename);
+                flags = O_RDONLY;
+                fd = create_file(filename, flags, mode);
+                if(fd < 0) { // error checking
+                    perror("open failed");
+                    return -1;
+                }
+                char fileContents[100];
+                ssize_t bytesRead = read_from_file(fd, fileContents, sizeof(fileContents) - 1);
+                if(bytesRead < 0) { // error checking
+                    perror("read failed");
+                    close_file(fd);
+                    return -1;
+                }
+                fileContents[bytesRead] = '\0'; // Null terminate fileContents
+                printf("file contents: %s\n", fileContents);
+                close_file(fd);
+                break;
+            case 4: // close a file
+                printf("What is the name of the file?\n");
+                scanf("%s", filename);
+                flags = O_WRONLY | O_CREAT | O_TRUNC;
+                fd = create_file(filename, flags, mode);
+                if(fd < 0) { // error checking
+                    perror("open failed");
+                    return -1;
+                }
+                close_file(fd);
+                break;
+            case 5:
+                printf("What is the name of the file?\n");
+                scanf("%s", filename);
+                delete_file(filename);
+                break;
+            case 6:
+                running = 0;
+                printf("Goodbye\n");
+                break;
+        }
     }
     return 0;
 }
--- Assignment_1/libfops.c
+++ Assignment_1/libfops.c
@@ -1,44 +1,57 @@
-#include <fops.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/syscall.h>
+#include <fcntl.h>
+#include "fops.h"
 
 int create_file(const char *filename, int flags, mode_t mode) //impements the functions using the syscall interface
 {
-    int fileDescriptor = syscall(SYS_open, filename, flags, mode);
-    if(fileDescriptor < 0) {
-        perror("open failed");
+    int fileDescriptor = syscall(SYS_open, filename, flags | O_CREAT, mode); // O_CREAT creates the file if it doesn't already exist
+    if(fileDescriptor < 0) { // error checking
+        perror("create failed");
         return -1;
     }
     else {
-        printf("file created\n");
+        printf("file created/opened\n");
     }
-    close(fileDescriptor);
-    return 0;
+    return fileDescriptor;
 }
 
 ssize_t write_to_file(int fd, const void *buf, size_t count)
 {
-    // syscall(SYS_write, ...);
-    printf("file written to\n");
-
+    ssize_t bytesWritten = syscall(SYS_write, fd, buf, count);
+    if(bytesWritten < 0) { // error checking
+        perror("write failed");
+        return -1;
+    } else {
+        printf("file written to\n");
+    }
+    return bytesWritten;
 }
 
 ssize_t read_from_file(int fd, void *buf, size_t count)
 {
-    // syscall(SYS_read, ...);
-    printf("file read\n");
+    ssize_t bytesRead = syscall(SYS_read, fd, buf, count);
+    if(bytesRead < 0) { // error checking
+        perror("read failed");
+        return -1;
+    } else {
+        printf("file read\n");
+    }
+    return bytesRead;
 }
 
 int close_file(int fd)
 {
-    // syscall(SYS_close, ...);
+    syscall(SYS_close, fd);
     printf("file closed\n");
+    return 0;
 }
 
 int delete_file(const char *filename)
 {
-    // syscall(SYS_unlink, ...);
+    syscall(SYS_unlink, filename);
     printf("file deleted\n");
+    return 0;
 }
